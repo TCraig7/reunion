3b6b0f3fd96192ea1d7ebcbddab34aeaa5d138f9
Tara Craig <tara.craig7@gmail.com>
2018-05-30 10:59:50 -0600

    Add participants, tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,26 @@
 class Activity
+
+  attr_reader   :name,
+                :participants
+
+  def initialize(name)
+    @name = name
+    @participants = []
+  end
+
+  def add_participant(participant)
+    @participants << participant
+  end
+
+  def calculate_base_cost
+    base_cost = []
+    @participants.map do |participant|
+      base_cost << participant[:paid]
+    end
+    base_cost.sum
+  end
+
+  def calculate_average_cost
+    calculate_base_cost/(@participants.count)    
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,55 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new(name)
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_attributes
+    activity = Activity.new("lunch")
+
+    assert_equal "lunch", activity.name
+  end
+
+  def test_it_has_participants
+    activity = Activity.new("lunch")
+
+    assert_equal [], activity.participants
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new("lunch")
+
+    expected = [{:name => "Debbie", :paid => 10}]
+
+    assert_equal expected, activity.add_participant({:name => "Debbie", :paid => 10})
+  end
+
+  def test_it_can_have_multiple_participants
+    activity = Activity.new("lunch")
+    activity.add_participant({:name => "Debbie", :paid => 20})
+
+    expected = [{:name=>"Debbie", :paid=>20}, {:name=>"Joe", :paid=>10}]
+
+    assert_equal expected, activity.add_participant({:name => "Joe", :paid => 10})
+  end
+
+  def test_it_can_calculate_base_cost
+    activity = Activity.new("lunch")
+    activity.add_participant({:name => "Debbie", :paid => 20})
+    activity.add_participant({:name => "Joe", :paid => 10})
+
+    assert_equal 30, activity.calculate_base_cost
+  end
+
+  def test_it_has_average_cost
+    activity = Activity.new("lunch")
+    activity.add_participant({:name => "Debbie", :paid => 20})
+    activity.add_participant({:name => "Joe", :paid => 10})
+
+    assert_equal 15, activity.calculate_average_cost
+  end
 end
